a715f5c406f48e308140cdaadcc14eb835dbc5ee
Philipp Keller <philipp.keller@ufz.de>
2018-05-29 13:30:15 +0200

    add functions to create ggplot2 ylabs for gasfluxes

---
+++ R/ggplot_shortcuts.R
@@ -0,0 +1,34 @@
+#' Label axis for CO2 according to flux scale.
+#'
+#' @param time character; Timescale. See 'Details'.
+#'
+#' @return ggplot ylab object.
+#' @export
+#'
+axis.flux.co2 <- function(time){
+  axis <- switch(time,
+                 "sec" = ylab(expression(CO[2]~Flux~µmol~m^-2~s^-1)),
+                 "day" = ylab(expression(CO[2]~Flux~mmol~m^-2~d^-1))
+  )
+}
+
+#' Label axis for arbitrary gases according to flux scale.
+#'
+#' @inheritParams axis.flux.co2
+axis.flux.gas <- function(time){
+  axis <- switch(time,
+                 "sec" = ylab(expression(Gas~Flux~µmol~m^-2~s^-1)),
+                 "day" = ylab(expression(Gas~Flux~mmol~m^-2~d^-1))
+  )
+}
+
+
+#' Label axis for CH4 according to flux scale.
+#'
+#' @inheritParams axis.flux.co2
+axis.flux.ch4 <- function(time){
+  axis <- switch(time,
+                 "sec" = ylab(expression(CH[4]~Flux~µmol~m^-2~s^-1)),
+                 "day" = ylab(expression(CH[4]~Flux~mmol~m^-2~d^-1))
+  )
+}

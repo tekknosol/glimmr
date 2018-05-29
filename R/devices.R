cfab2778a80dc30fe75d349bf89e009c77f88941
Philipp Keller <philipp.keller@ufz.de>
2018-05-29 22:39:01 +0200

    add example for gasmet data to readme and preprocess_gasmet()

--- R/devices.R
+++ R/devices.R
@@ -8,8 +8,10 @@
 #' @return dataframe which can be used as input of the gasfluxes function
 #' @export
 #'
+#' @examples
+#' flux_raw <- preprocess_gasmet(gasmet, meta)
 preprocess_gasmet <- function(gasmet, meta, V = 0.01461, A = 0.098) {
-    hmr.data <- data.frame(spot = NA, day = NA, rep = NA, V = NA, A = NA, Time = NA, Concentration = NA, CH4mmol = NA, 
+    hmr.data <- data.frame(spot = NA, day = NA, rep = NA, V = NA, A = NA, Time = NA, Concentration = NA, CH4mmol = NA,
         co2 = NA, ch4 = NA)
     for (i in 1:length(meta$spot)) {
         if (!is.na(meta[i, ]$begin)) {
@@ -18,10 +20,10 @@ preprocess_gasmet <- function(gasmet, meta, V = 0.01461, A = 0.098) {
             a <- gasmet[(begin + meta[i, ]$offset):(begin + meta[i, ]$wndw - 1), ]
             conc <- a$CO2 * 10^-6 * (meta[i, ]$pmbar * 100)/(8.314 * (meta[i, ]$temp + 273.15)) * 1000  # calculate concentration in mmol/m³
             conc.CH4 <- a$CH4 * 10^-6 * (meta[i, ]$pmbar * 100)/(8.314 * (meta[i, ]$temp + 273.15)) * 1000
-            
+
             # time in hour because algorithm expects that. fluxes need to be trensformed to daily fluxes afterwards
-            hmr.data.tmp <- data.frame(spot = meta[i, ]$spot, day = meta[i, ]$day, rep = rownames(meta[i, ]), 
-                V = V, A = A, Time = as.numeric(a$datetime - a[1, ]$datetime)/60/60, Concentration = conc, CH4mmol = conc.CH4, 
+            hmr.data.tmp <- data.frame(spot = meta[i, ]$spot, day = meta[i, ]$day, rep = rownames(meta[i, ]),
+                V = V, A = A, Time = as.numeric(a$datetime - a[1, ]$datetime)/60/60, Concentration = conc, CH4mmol = conc.CH4,
                 ch4 = "ch4", co2 = "co2")
             hmr.data <- rbind(hmr.data, hmr.data.tmp)
         }
--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
+    ## 
+    ## Attaching package: 'dplyr'
+
+    ## Die folgenden Objekte sind maskiert von 'package:stats':
+    ## 
+    ##     filter, lag
+
+    ## Die folgenden Objekte sind maskiert von 'package:base':
+    ## 
+    ##     intersect, setdiff, setequal, union
+
 TregaTa
 =======
 
@@ -16,4 +27,25 @@ Installation
 # Development version from GitHub:
 # install.packages("devtools")
 devtools::install_github("tekknosol/tregata")
+```
+
+Usage
+-----
+
+``` r
+data("gasmet", "meta")
+flux_raw <- preprocess_gasmet(gasmet, meta)
+glimpse(flux_raw)
+#> Observations: 150
+#> Variables: 10
+#> $ spot          <chr> "BZT", "BZT", "BZT", "BZT", "BZT", "BZT", "BZT",...
+#> $ day           <chr> "08.05.2018", "08.05.2018", "08.05.2018", "08.05...
+#> $ rep           <chr> "1", "1", "1", "1", "1", "1", "1", "1", "1", "1"...
+#> $ V             <dbl> 0.01461, 0.01461, 0.01461, 0.01461, 0.01461, 0.0...
+#> $ A             <dbl> 0.098, 0.098, 0.098, 0.098, 0.098, 0.098, 0.098,...
+#> $ Time          <dbl> 0.000000000, 0.008333333, 0.016666667, 0.0250000...
+#> $ Concentration <dbl> 16.83826, 16.56487, 16.39089, 16.22935, 16.11750...
+#> $ CH4mmol       <dbl> 0.09030110, 0.08657308, 0.09154378, 0.08988688, ...
+#> $ co2           <chr> "co2", "co2", "co2", "co2", "co2", "co2", "co2",...
+#> $ ch4           <chr> "ch4", "ch4", "ch4", "ch4", "ch4", "ch4", "ch4",...
 ```

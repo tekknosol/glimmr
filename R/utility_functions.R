972a03a6c163d66c183476d84bbf2fb71eed9fc9
Philipp Keller <philipp.keller@ufz.de>
2018-05-29 10:16:55 +0200

    change function names according to name convention and keep old functions as deprecated

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -7,7 +7,7 @@
 #' @return test
 #' @export
 #'
-calc.SN <- function(wt, gas = "co2") {
+calc_SN <- function(wt, gas = "co2") {
     switch(tolower(gas), co2 = {
         SN <- 1923.6 + (-125.06 * wt) + (4.3773 * wt^2) + (-0.085681 * wt^3) + (0.00070284 * wt^4)
     }, ch4 = {
@@ -15,10 +15,27 @@ calc.SN <- function(wt, gas = "co2") {
     })
 }
 
-calc.u10 <- function(ws) {
+#' @inheritParams calc_SN
+calc.SN <- function(wt, gas = "co2") {
+  .Deprecated("calc_SN")
+  switch(tolower(gas), co2 = {
+    SN <- 1923.6 + (-125.06 * wt) + (4.3773 * wt^2) + (-0.085681 * wt^3) + (0.00070284 * wt^4)
+  }, ch4 = {
+    SN <- 1909.4 + (-120.78 * wt) + (4.1555 * wt^2) + (-0.080578 * wt^3) + (0.00065777 * wt^4)
+  })
+}
+
+calc_u10 <- function(ws) {
     u10 <- (1.22 * ws)
     # u10 <- ws * 10^0.16
     return(u10)
+}
+
+calc.u10 <- function(ws) {
+  .Deprecated("calc_u10")
+  u10 <- (1.22 * ws)
+  # u10 <- ws * 10^0.16
+  return(u10)
 }
 
 #' Calculate Gas transfer coefficient
@@ -36,7 +53,7 @@ calc.u10 <- function(ws) {
 #' @references Wanninkhof, R., 2014. Relationship between wind speed and gas exchange over the ocean revisited. Limnology and Oceanography: Methods 12, 351–362. https://doi.org/10.4319/lom.2014.12.351
 #' @export
 #'
-calc.kW <- function(ws, wt, gas = "co2", model = "wann14") {
+calc_kW <- function(ws, wt, gas = "co2", model = "wann14") {
     switch(tolower(model), wann14 = {
         kW <- switch(tolower(gas), co2 = {
             0.251 * calc.u10(ws)^2 * (calc.SN(wt, gas)/660)^-0.5
@@ -46,3 +63,16 @@ calc.kW <- function(ws, wt, gas = "co2", model = "wann14") {
     })
     return(kW)
 }
+
+#' @inheritParams calc_kW
+calc.kW <- function(ws, wt, gas = "co2", model = "wann14") {
+  switch(tolower(model), wann14 = {
+    kW <- switch(tolower(gas), co2 = {
+      0.251 * calc.u10(ws)^2 * (calc.SN(wt, gas)/660)^-0.5
+    }, ch4 = {
+      0.251 * calc.u10(ws)^2 * (calc.SN(wt, gas)/660)^-0.5
+    })
+  })
+  return(kW)
+}
+

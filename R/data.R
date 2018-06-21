14059ca150610741df84bc6690c493c1db65cc4a
Philipp Keller <philipp.keller@ufz.de>
2018-06-21 13:08:15 +0200

    add losgatos data

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' GASMET Flux measurement with three entrys.
+#' GASMET Flux measurement with three entries.
 #'
 #' A dataset containing three flux measurements.
 #'
@@ -14,6 +14,34 @@
 #' }
 "gasmet"
 
+#' LosGatos Flux measurement with three entries.
+#'
+#' A dataset containing one flux measurements.
+#'
+#' @format A data frame with 1684 rows and 5 variables:
+#' \describe{
+#'   \item{Time}{Datetime of measurement}
+#'   \item{[CH4]_ppm}{}
+#'   \item{[CO2]_ppm}{}
+#'   \item{GasP_torr}{}
+#'   \item{AmbT_C}{}
+#' }
+"losgatos"
+
+#' Metafile for LosGatos measurement.
+#'
+#' A dataset containing the meta information for one flux measurements.
+#'
+#' @format A data frame with 1 rows and 19 variables:
+#' \describe{
+#'   \item{spot}{ID of the measurement spot}
+#'   \item{day}{Day of the measurement}
+#'   \item{start}{}
+#'   \item{end}{}
+#'   \item{t_start}{}
+#'   \item{t_end}{}
+#' }
+"meta_losgatos"
 
 #' Metafile for GASMET measurement.
 #'
--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -5,15 +5,24 @@ gasmet <- read_gasmet("data-raw/gasmet.csv")
 meta_gasmet <- readr::read_csv("data-raw/meta.csv")
 
 meta_gasmet <- meta_gasmet %>%
-  mutate(begin=dmy_hms(paste(day,begin))) %>%
+  mutate(begin = dmy_hms(paste(day, begin))) %>%
   select(spot, day, begin, temp, wndw, offset)
 
-meta_gasmet <- meta_gasmet[1:6,]
+meta_gasmet <- meta_gasmet[1:6, ]
 
-mask <- which((gasmet$datetime>=meta_gasmet$begin[1]&gasmet$datetime <= meta_gasmet$begin[3])|(gasmet$datetime>=meta_gasmet$begin[4]&gasmet$datetime <= meta_gasmet$begin[6]))
+mask <- which( (gasmet$datetime >= meta_gasmet$begin[1] &
+  gasmet$datetime <= meta_gasmet$begin[3]) |
+  (gasmet$datetime >= meta_gasmet$begin[4] &
+  gasmet$datetime <= meta_gasmet$begin[6])
+)
 
-mask <- append(mask,(mask[length(mask)]+1):(mask[length(mask)]+15))
+mask <- append(mask, (mask[length(mask)] + 1):(mask[length(mask)] + 15))
 
-gasmet <- gasmet[mask,]
+gasmet <- gasmet[mask, ]
 
-devtools::use_data(gasmet, meta_gasmet, overwrite = T)
+meta_losgatos <- readr::read_csv("data-raw/meta_losgatos.csv")
+meta_losgatos <- meta_losgatos %>%
+  mutate(start = ymd_hms(paste(day, start)), end = ymd_hms(paste(day, end)))
+losgatos <- read_losgatos("data-raw/2018-05-23/")
+
+devtools::use_data(gasmet, meta_gasmet, losgatos, meta_losgatos, overwrite = T)

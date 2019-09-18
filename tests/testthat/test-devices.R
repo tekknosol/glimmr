720f3aafe180b653f898f460fff43d154b60b836
Philipp Keller <philipp.keller@ufz.de>
2019-09-18 16:31:29 +0200

    improve code

--- tests/testthat/test-devices.R
+++ tests/testthat/test-devices.R
@@ -2,6 +2,7 @@ context("Devices")
 library(glimmr)
 library(dplyr)
 
+# test fluxcalculation with losgatos
 test_that("LosGatos", {
   Flux_a1_1 <- process_losgatos(losgatos, meta_losgatos) %>% filter(site == "A1", rep == 1)
   expect_equal(Flux_a1_1 %>% filter(gas == "CO2") %>% pull("F_LM"), 20.12128, tolerance=1e-3)
@@ -10,6 +11,7 @@ test_that("LosGatos", {
   expect_equal(Flux_a1_1 %>% filter(gas == "CH4") %>% pull("F_RLM"), 0.007568566, tolerance=1e-3)
 })
 
+# test fluxcalculation with gasmet
 test_that("GASMET", {
   Flux_a1_1 <- process_gasmet(gasmet, meta_gasmet) %>% filter(site == "YTMeteo", rep == 1)
   expect_equal(Flux_a1_1 %>% filter(gas == "CO2") %>% pull("F_LM"), 25.90705, tolerance=1e-3)

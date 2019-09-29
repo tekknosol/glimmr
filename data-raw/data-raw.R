a56e15ee76b6cf1132d0889ec700474cfb0c8c27
Philipp Keller <philipp.keller@ufz.de>
2019-09-29 20:44:15 +0200

    update data

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -11,12 +11,14 @@ meta_gasmet <- meta_gasmet %>%
 meta_gasmet <- meta_gasmet[1:6, ]
 meta_gasmet$day <- ymd("2017-04-12")
 meta_gasmet$start <- as.character(meta_gasmet$start)
+meta_gasmet$spot <- rep(c("plotA", "plotB"), each = 3)
 
 meta_losgatos <- readr::read_csv("data-raw/meta_losgatos.csv")
 meta_losgatos$start <- as.character(meta_losgatos$start)
 meta_losgatos$end <- as.character(meta_losgatos$end)
+meta_losgatos$spot <- "plotA"
 # meta_losgatos <- meta_losgatos %>%
   # mutate(start = ymd_hms(paste(day, start)), end = ymd_hms(paste(day, end)))
 losgatos <- read_losgatos("data-raw/2018-05-23/")
 
-devtools::use_data(gasmet, meta_gasmet, losgatos, meta_losgatos, overwrite = T)
+usethis::use_data(gasmet, meta_gasmet, losgatos, meta_losgatos, overwrite = T)
